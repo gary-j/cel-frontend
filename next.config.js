b345c823549b7ca8b850d7593f12d6477953c766
gary-j <gary.j@live.fr>
2022-05-11 12:08:05 +0200

    Initialize frontEnd, Set PWA, manifest.json, next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPwa = require('next-pwa');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPwa({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+});
+
+module.exports = nextConfig;
