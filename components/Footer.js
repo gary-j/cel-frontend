3806a2842e8a358e8a3a1503a9ccf284a818f03f
gary-j <gary.j@live.fr>
2022-05-15 01:21:23 +0200

    signin et sign up workin with Gwendal front-end, basic of Next : css, pages, routes, 404, Layout component

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Citron en limonade © 2022</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/auth/signin.js
@@ -0,0 +1,9 @@
+const signin = () => {
+  return (
+    <div>
+      <h1>Sign in Page</h1>
+    </div>
+  );
+};
+
+export default signin;
