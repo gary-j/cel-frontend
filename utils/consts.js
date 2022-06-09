a5ede77f188323f71ffc3ef7f95ed5499e49d34e
gary-j <gary.j@live.fr>
2022-06-09 15:39:10 +0200

    fix google font loading and add ENV prod/dev variable to simplify backend_urm

--- utils/consts.js
+++ utils/consts.js
@@ -1,2 +1,4 @@
 export const BACKEND_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5005/api';
+  process.env.ENV === 'PROD'
+    ? process.env.NEXT_PUBLIC_BACKEND_URL
+    : 'http://localhost:5005/api';
