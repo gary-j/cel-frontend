30e1f90a698ea5a9571fb3197ba6ff8bc3387772
gary-j <gary.j@live.fr>
2022-06-09 19:07:44 +0200

    undo BACKEND_URL changes in consts.js

--- utils/consts.js
+++ utils/consts.js
@@ -1,4 +1,2 @@
 export const BACKEND_URL =
-  process.env.ENV === 'PROD'
-    ? process.env.NEXT_PUBLIC_BACKEND_URL
-    : 'http://localhost:5005/api';
+  process.env.BACKEND_URL || 'http://localhost:5005/api';
