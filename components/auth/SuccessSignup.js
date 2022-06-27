553b980c611f6c75639af3de960e74f6c33f58fd
gary-j <gary.j@live.fr>
2022-06-27 16:57:14 +0200

    Progress bar ok

--- components/auth/SuccessSignup.js
+++ components/auth/SuccessSignup.js
@@ -13,9 +13,7 @@ function SuccessSignup(props) {
           <p className={styles.p2}>
             Confirmez votre adresse email via le lien envoyé à
           </p>
-          <p className={styles.p2 + ' ' + styles.email}>
-            email props : {props?.user?.email}
-          </p>
+          <p className={styles.p2 + ' ' + styles.email}>{props?.user?.email}</p>
           <p className={styles.p2}>Merci et à bientôt !</p>
         </div>
         <button
