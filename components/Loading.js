b83857e5aa3ef8ed6c480ebf081f8b02b532993c
gary-j <gary.j@live.fr>
2022-06-29 15:16:23 +0200

    Loading pop up and dynamic message for signup

--- components/Loading.js
+++ components/Loading.js
@@ -3,16 +3,21 @@ import styles from './Loading.module.scss';
 import Image from 'next/image';
 
 const Loading = (props) => {
+  console.log('*** Loading.js PROPS *** : ', props);
+  let instruction = props.instruction;
+  const message = () => {};
   return (
-    <div className={styles.loading}>
-      <p className={styles.p}>Authentification en cours...</p>
-      <Image
-        src='/logos/logo-green-light-2-.png'
-        alt='image chargement - citron en limonade'
-        width={50}
-        height={50}
-        className={styles.image}
-      />
+    <div className={styles.container}>
+      <div className={styles.loading}>
+        <p className={styles.p}>Chargement en cours...</p>
+        <Image
+          src='/logos/logo-green-light-2-.png'
+          alt='image chargement - citron en limonade'
+          width={50}
+          height={50}
+          className={styles.image}
+        />
+      </div>
     </div>
   );
 };
