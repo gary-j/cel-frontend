fa3c103342b9dcb180cb61aa4c33de5f7556b333
gary-j <gary.j@live.fr>
2022-08-05 18:45:38 +0200

    test deploy again

--- context/breakPoints.context.js
+++ context/breakPoints.context.js
@@ -12,7 +12,7 @@ const breakPoints = {
 
 //
 function BreakPointProviderWrapper(props) {
-  const [breakPoint, setBreakPoint] = useState();
+  const [breakPoint, setBreakPoint] = useState('(min-width:1440px)');
   useEffect(() => {
     breakPointObserver(breakPoints, setBreakPoint);
   }, [breakPoint]);
