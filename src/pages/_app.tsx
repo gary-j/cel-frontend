3b1325b9c884c39b435130cda2f4e360522c52c4
GWilczyk <gwilczyk.github@gmail.com>
2022-12-20 19:55:38 +0100

    Adding TailwindCSS Support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,26 @@
-import Layout from '../components/commons/Layout'
-import '../styles/globals.scss'
+// import Layout from '../components/commons/Layout'
+// import '../styles/globals.scss'
 // import '../styles/test.scss';
-
+import '@styles/globals.css'
 import { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import { AuthProviderWrapper } from '../../context/auth.context'
-import { BreakPointProviderWrapper } from '../../context/breakPoints.context'
+// import { AuthProviderWrapper } from '../../context/auth.context'
+// import { BreakPointProviderWrapper } from '../../context/breakPoints.context'
 
 const queryClient = new QueryClient()
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<AuthProviderWrapper>
-				<BreakPointProviderWrapper>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</BreakPointProviderWrapper>
-			</AuthProviderWrapper>
-
+			{/* <AuthProviderWrapper> */}
+			{/* <BreakPointProviderWrapper> */}
+			{/* <Layout> */}
+			<Component {...pageProps} />
 			<ReactQueryDevtools initialIsOpen={false} />
+			{/* </Layout> */}
+			{/* </BreakPointProviderWrapper> */}
+			{/* </AuthProviderWrapper> */}
 		</QueryClientProvider>
 	)
 }
